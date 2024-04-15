75bae28add13d5dfcf8e4249a87cec899604ec38
gstrand17 <gstrand@ufl.edu>
2024-04-15 16:14:53 -0400

    command line UI beginnings

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 using namespace std;
 
 int main(){
-    cout << "Welcome to CITY HUNT!"
+    cout << "Welcome to CITY SEEKER!\nHelp us help you chose your dream city!"<< endl;
+    cout << "What is your name?" << endl;
+    string name;
+    cin >> name;
+    cout << "Hi there " << name << "!" << endl;
+    cout << "Enter your preferences for different categories and I will calculate the ideal city for you!" << endl;
+    vector<int> populationOptions;
+
     //tryna figure this out
 }
