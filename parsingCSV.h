676e7c2c686b186367bc604d2e61b18a1cf12da6
Huliooo611 <julioarboleda@ufl.edu>
2024-04-16 14:16:17 -0400

    your mom hot karmen

--- parsingCSV.h
+++ parsingCSV.h
@@ -1,5 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ifstream>
 
-class parsingCSV {
-	//Graciela looks like a dude on meth
+struct city
+{
+    string name;
+    string state;
+    int population;
+    float area;
+    float average_age;
+    float average_household_income;
+    string climate;
+    // Any reccommendations?
+    //https://data.census.gov/table/ACSDP5Y2022.DP05?g=040XX00US12$1600000&d=ACS%205-Year%20Estimates%20Data%20Profiles&moe=false
+    //The link above is to the data set just for Florida so lmk if u like it also dont download table just view it and make sure to get rid of margin of error
+};
+
+class parsingCSV
+        {
+    private:
+        vector<city> cities;
+    public:
+
 };
 
