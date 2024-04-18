58c6f979e53b7f410f4d70fefe3f312cfa97f114
gstrand17 <gstrand@ufl.edu>
2024-04-18 15:25:05 -0400

    added my heap stuff in the main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "parsingCSV.h"
 #include "hashTable.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ using namespace std;
 
 
 
-vector<string> citySearcher(string cityName, hashMapLinear& myMap){
+vector<string> citySearcher(string cityName, hashMapLinear& myMap, priority_queue<pair<int, string>> maxPop, priority_queue<pair<int, string>> maxDen, priority_queue<pair<int, string>> maxSize, priority_queue<pair<int, string>> maxAge){
     //RETRIEVAL
     vector<string> data;
     data = myMap.find(cityName);
@@ -23,42 +24,75 @@ vector<string> citySearcher(string cityName, hashMapLinear& myMap){
         cout << "Median Age: " << data[5] <<  endl;
 
     }
+    //pushes all the new data in a <value, cityname> pair onto each of the 4 heaps
     pair<int, string> dataPairs;
     dataPairs.first = stoi(data[2]);
     dataPairs.second = cityName;
-    //insert population into pop heap
+    maxPop.push(dataPairs);
+
     dataPairs.first = stoi(data[3]);
-    //insert pop density into den heap
+    maxDen.push(dataPairs);
+
     dataPairs.first = stoi(data[4]);
-    //insert hhsize into size heap
+    maxSize.push(dataPairs);
+
     dataPairs.first = stoi(data[5]);
-    //insert median age into age heap
+    maxAge.push(dataPairs);
 
     return data;
 }
 
-void Rank(string option, hashMapLinear& myMap){
+void Rank(string option, priority_queue<pair<int, string>> maxPop, priority_queue<pair<int, string>> maxDen, priority_queue<pair<int, string>> maxSize, priority_queue<pair<int, string>> maxAge){
     int counter = 1;
     if (option == "1"){
         cout << "Population Ranking (greatest to least)" << endl;
         //for population in all cities, cout << counter << city name << population, counter ++
+        while (!maxPop.empty()){
+            //next two lines inspired by https://www.geeksforgeeks.org/priority-queue-of-pairs-in-c-ordered-by-first/
+            pair<int, string> top = maxPop.top();
+            cout << counter << ". " << top.second << " = " << top.first<< endl;
+            maxPop.pop();
+            counter++;
+        }
+        exit(1);
     }
     else if (option == "2"){
         cout << "Population Density Ranking (greatest to least)" << endl;
+        while (!maxDen.empty()){
+            pair<int, string> top = maxDen.top();
+            cout << counter << ". " << top.second << " = " << top.first<< endl;
+            maxDen.pop();
+            counter++;
+        }
+        exit(1);
     }
     else if (option == "3"){
-        cout << "Median Age Ranking (greatest to least)" << endl;
+        cout <<  "Average Household Size Ranking (greatest to least)" << endl;
+        while (!maxSize.empty()){
+            pair<int, string> top = maxSize.top();
+            cout << counter << ". " << top.second << " = " << top.first<< endl;
+            maxSize.pop();
+            counter++;
+        }
+        exit(1);
     }
     else if (option == "4"){
-        cout << "Average Household Size Ranking (greatest to least)" << endl;
+        cout << "Median Age Ranking (greatest to least)" << endl;
+        while (!maxAge.empty()){
+            pair<int, string> top = maxAge.top();
+            cout << counter << ". " << top.second << " = " << top.first<< endl;
+            maxAge.pop();
+            counter++;
+        }
+        exit(1);
     }
     else{
         cout << "invalid selection :(" << endl;
-        //menuBasic(myMap);
+        exit(1);
     }
 }
 
-void menuBasic(hashMapLinear& myMap){
+void menuBasic(hashMapLinear& myMap, priority_queue<pair<int, string>> maxPop, priority_queue<pair<int, string>> maxDen, priority_queue<pair<int, string>> maxSize, priority_queue<pair<int, string>> maxAge){
     cout << "Pick from the following options: " << endl;
     cout << "1 - Search another city" << endl;
     cout << "2 - Rank Cities" << endl;
@@ -69,16 +103,16 @@ void menuBasic(hashMapLinear& myMap){
     if (option == "1") {
         cout << "Enter the name of another city: " << endl;
         cin >> cityName;
-        citySearcher(cityName, myMap);
-        menuBasic(myMap);
+        citySearcher(cityName, myMap, maxPop, maxDen, maxSize, maxAge);
+        menuBasic(myMap, maxPop, maxDen, maxSize, maxAge);
     }
     else if (option == "2"){
         cout << "1 - Rank by Population" << endl;
         cout << "2 - Rank by Density" << endl;
-        cout << "3 - Rank by Age" << endl;
-        cout << "4 - Rank by Household Size" << endl;
+        cout << "3 - Rank by Household Size" << endl;
+        cout << "4 - Rank by Median Age" << endl;
         cin >> option;
-        Rank(option, myMap);
+        Rank(option, maxPop, maxDen, maxSize, maxAge);
 
     }
     else if (option == "3"){
@@ -86,16 +120,20 @@ void menuBasic(hashMapLinear& myMap){
     }
     else{
         cout << "invalid selection :(" << endl;
-        menuBasic(myMap);
+        menuBasic(myMap, maxPop, maxDen, maxAge, maxSize);
     }
 }
 
 
 
 int main() {
-    //for line in data set, call parse function
     hashMapLinear LinearMap(10);
     parseCSV(LinearMap);
+
+    priority_queue<pair<int, string>> maxPop;
+    priority_queue<pair<int, string>> maxDen;
+    priority_queue<pair<int, string>> maxSize;
+    priority_queue<pair<int, string>> maxAge;
 
     cout << "Welcome to CITY SEARCHER!\nThis program will generate numerical demographical information for any USA city"<< endl;
     cout << "What is your name?" << endl;
@@ -105,11 +143,11 @@ int main() {
     cout << "Enter the name of a city: " << endl;
     string cityName;
     cin >> cityName;
-    citySearcher(cityName, LinearMap);
+    citySearcher(cityName, LinearMap, maxPop, maxDen, maxSize, maxAge);
     cout << "Enter the name of another city: " << endl;
     cin >> cityName;
-    citySearcher(cityName, LinearMap);
-    menuBasic(LinearMap);
+    citySearcher(cityName, LinearMap, maxPop, maxDen, maxSize, maxAge);
+    menuBasic(LinearMap, maxPop, maxDen, maxSize, maxAge);
 
 
     return 0;
