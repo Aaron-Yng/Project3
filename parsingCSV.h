b8758fc712633663aa52727cebc9e7adc99b0c9d
Carmen Cruz Arana <carmenrubi.arana@gmail.com>
2024-04-18 12:11:28 -0400

    parsing function!

--- parsingCSV.h
+++ parsingCSV.h
@@ -1,18 +1,25 @@
 #include <iostream>
+#include "hashTable.h"
 #include <vector>
+#include <fstream>
 #include <string>
+#include <sstream>
 #include <fstream>
 using namespace std;
 
-class parsingCSV
-{
-    private:
-        vector<vector<string>> cities;
-
-    public:
-        vector<vector<string>> parseCitiesData()
-        {
-
-        };
-};
+vector<string> parseCSV(hashMapLinear map){
+    fstream cityInfo;
+    string line;
+    string attribute;
+    vector<string> attributes;
+    cityInfo.open("uscities.csv",ios::in);
+    while(!cityInfo.eof()){
+        getline(cityInfo,line);
+        stringstream s(line);
+        while(getline(s,attribute,',')){
+            attributes.push_back(attribute);
+        }
+        map.insert(attributes);
+    }
+}
 
