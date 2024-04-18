3d5e704655be53251c833ea2eab7e2a3f201fa12
Huliooo611 <julioarboleda@ufl.edu>
2024-04-18 13:23:00 -0400

    Updated and Fixed Parsing Function in parsingCSV

--- parsingCSV.h
+++ parsingCSV.h
@@ -9,15 +9,18 @@ using namespace std;
 
 vector<string> parseCSV(hashMapLinear map){
     fstream cityInfo;
-    string line;
-    string attribute;
+    string line, attribute;
     vector<string> attributes;
     cityInfo.open("uscities.csv",ios::in);
+    getline(cityInfo, line);
     while(!cityInfo.eof()){
+        int iteration = 0;
         getline(cityInfo,line);
         stringstream s(line);
         while(getline(s,attribute,',')){
-            attributes.push_back(attribute);
+            if(iteration == 0 || iteration == 1 || iteration == 6 || iteration == 7 || iteration == 12 || iteration == 13)
+                attributes.push_back(attribute);
+            iteration++;
         }
         map.insert(attributes);
     }
