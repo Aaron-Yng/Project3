81c7f18c32710b4a8bd9262b1ad1c4c5c2d88275
Aaron Young <froakbeats@gmail.com>
2024-12-01 16:45:04 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Aaron Young on 12/1/24.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    //stuff here
+    cout << "first commit test" << endl;
+}
